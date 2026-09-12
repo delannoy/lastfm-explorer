86438764ce4abe4ecb0448478d731a406aae7a2b
Andres G. Delannoy <delannoy@cern.ch>
2026-09-12 18:48:34 +0200

    minor refactoring; added yr.no forecast urls

--- nowplaying/nowplaying.py
+++ nowplaying/nowplaying.py
@@ -14,17 +14,33 @@ import urllib.request
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(message)s')
 
-PORT = 3000
-
-LATITUDE = float(os.environ.get('LOCATION_LATITUDE', 46.23424))
-LONGITUDE = float(os.environ.get('LOCATION_LONGITUDE', 6.08025))
-
 LASTFM_KEY = os.environ.get('LASTFM_KEY', '').replace('-', '')
 LASTFM_USER = os.environ.get('LASTFM_USER', 'adelannoy')
 
 if not LASTFM_KEY:
     logging.error('Error: LASTFM_KEY environment variable must be set (https://www.last.fm/api/account/create to create one or https://www.last.fm/api/accounts to view existing ones).')
     sys.exit(1)
+
+PORT = 3000
+INDEX_HTML = (pathlib.Path(__file__).parent / 'index.html').read_bytes()
+
+LATITUDE = float(os.environ.get('LOCATION_LATITUDE', 46.23424))
+LONGITUDE = float(os.environ.get('LOCATION_LONGITUDE', 6.08025))
+WEATHER_CODES = { # https://open-meteo.com/en/docs#weather_variable_documentation
+    0: ('Clear sky', '☀️'),
+    1: ('Mainly clear', '🌤️'), 2: ('Partly cloudy', '⛅'), 3: ('Overcast', '☁️'),
+    45: ('Fog', '🌫️'), 48: ('Depositing rime fog', '🌫️'),
+    51: ('Light drizzle', '🌧️'), 53: ('Moderate drizzle', '🌧️'), 55: ('Dense drizzle', '🌧️'),
+    56: ('Light freezing drizzle', '🌧️'), 57: ('Dense freezing drizzle', '🌧️'),
+    61: ('Slight rain', '🌧️'), 63: ('Moderate rain', '🌧️'), 65: ('Heavy rain', '🌧️'),
+    66: ('Light freezing rain', '🌧️'), 67: ('Heavy freezing rain', '🌧️'),
+    71: ('Slight snow fall', '❄️'), 73: ('Moderate snow fall', '❄️'), 75: ('Heavy snow fall', '❄️'),
+    77: ('Snow grains', '❄️'),
+    80: ('Slight rain showers', '🌦️'), 81: ('Moderate rain showers', '🌦️'), 82: ('Violent rain showers', '🌦️'),
+    85: ('Slight snow showers', '🌨️'), 86: ('Heavy snow showers', '🌨️'),
+    95: ('Thunderstorm', '⛈️'),
+    96: ('Thunderstorm, slight hail', '⛈️'), 99: ('Thunderstorm, heavy hail', '⛈️'),
+}
 
 def query(url: str) -> dict:
     '''Query and parse JSON with basic error handling.'''
@@ -41,7 +57,8 @@ def query(url: str) -> dict:
 
 def lastfm_nowplaying(user: str, api_key: str) -> dict[str, int|bool|str]:
     '''Query and parse the latest listen from LASTFM.'''
-    url = f'https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user={user}&api_key={api_key}&format=json&limit=1'
+    params = dict(method='user.getrecenttracks', user=user, api_key=api_key, format='json', limit=1)
+    url = f'https://ws.audioscrobbler.com/2.0/?{urllib.parse.urlencode(params)}'
     data = query(url=url)
     if 'error' in data:
         return data
@@ -54,6 +71,7 @@ def lastfm_nowplaying(user: str, api_key: str) -> dict[str, int|bool|str]:
     artist_mbid, album_mbid, track_mbid = track.get('artist', {}).get('mbid', ''), track.get('album', {}).get('mbid', ''), track.get('mbid', '')
     stats = lastfm_track_stats(user, api_key, track_name, artist_name)
     return {
+        'user': user,
         'playing': (track.get('@attr', {}).get('nowplaying') == 'true'),
         'artist': artist_name,
         'album': album_name,
@@ -62,11 +80,11 @@ def lastfm_nowplaying(user: str, api_key: str) -> dict[str, int|bool|str]:
         'total_listens': data.get('recenttracks', {}).get('@attr', {}).get('total', 0),
         'track_listens': stats['track_listens'],
         'loved': stats['loved'],
-        'user_url': f"https://www.last.fm/user/{urllib.parse.quote_plus(user)}",
+        'user_url': f'https://www.last.fm/user/{urllib.parse.quote_plus(user)}',
         'artist_url': stats['artist_url'],
         'album_url': stats['album_url'],
         'track_url': stats['track_url'],
-        'track_library_url': f"https://www.last.fm/user/{urllib.parse.quote_plus(user)}/library/music/{urllib.parse.quote_plus(artist_name)}/_/{urllib.parse.quote_plus(track_name)}",
+        'track_library_url': f'https://www.last.fm/user/{urllib.parse.quote_plus(user)}/library/music/{urllib.parse.quote_plus(artist_name)}/_/{urllib.parse.quote_plus(track_name)}',
         'artist_mb_url': f'https://musicbrainz.org/artist/{artist_mbid}' if artist_mbid else '',
         'album_mb_url': f'https://musicbrainz.org/release/{album_mbid}' if album_mbid else '',
         'track_mb_url': f'https://musicbrainz.org/recording/{track_mbid}' if track_mbid else '',
@@ -77,7 +95,8 @@ def lastfm_track_stats(user: str, api_key: str, track_name: str, artist_name: st
     default_stats = {'track_listens': 0, 'loved': False, 'track_url': '', 'artist_url': '', 'album_url': ''}
     if not track_name or not artist_name:
         return default_stats
-    url = f'https://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key={api_key}&artist={urllib.parse.quote(artist_name)}&track={urllib.parse.quote(track_name)}&username={user}&format=json'
+    params = dict(method='track.getInfo', user=user, api_key=api_key, format='json', artist=urllib.parse.quote(artist_name), track=urllib.parse.quote(track_name))
+    url = f'https://ws.audioscrobbler.com/2.0/?{urllib.parse.urlencode(params)}'
     data = query(url=url)
     if 'error' in data:
         return default_stats
@@ -87,7 +106,7 @@ def lastfm_track_stats(user: str, api_key: str, track_name: str, artist_name: st
         'loved': (int(track_info.get('userloved', 0)) == 1),
         'track_url': track_info.get('url', ''),
         'artist_url': track_info.get('artist', {}).get('url', ''),
-        'album_url': track_info.get('album', {}).get('url', '')
+        'album_url': track_info.get('album', {}).get('url', ''),
     }
 
 @functools.lru_cache(maxsize=1)
@@ -100,25 +119,6 @@ def resolve_location(latitude: float, longitude: float) -> str:
     addr = data.get('address', {})
     return addr.get('city') or addr.get('town') or addr.get('village') or addr.get('county') or 'Unknown'
 
-def weather_description(code: int) -> tuple[str, str]:
-    '''https://open-meteo.com/en/docs#weather_variable_documentation'''
-    codes = {
-        0: ('Clear sky', '☀️'),
-        1: ('Mainly clear', '🌤️'), 2: ('Partly cloudy', '⛅'), 3: ('Overcast', '☁️'),
-        45: ('Fog', '🌫️'), 48: ('Depositing rime fog', '🌫️'),
-        51: ('Light drizzle', '🌧️'), 53: ('Moderate drizzle', '🌧️'), 55: ('Dense drizzle', '🌧️'),
-        56: ('Light freezing drizzle', '🌧️'), 57: ('Dense freezing drizzle', '🌧️'),
-        61: ('Slight rain', '🌧️'), 63: ('Moderate rain', '🌧️'), 65: ('Heavy rain', '🌧️'),
-        66: ('Light freezing rain', '🌧️'), 67: ('Heavy freezing rain', '🌧️'),
-        71: ('Slight snow fall', '❄️'), 73: ('Moderate snow fall', '❄️'), 75: ('Heavy snow fall', '❄️'),
-        77: ('Snow grains', '❄️'),
-        80: ('Slight rain showers', '🌦️'), 81: ('Moderate rain showers', '🌦️'), 82: ('Violent rain showers', '🌦️'),
-        85: ('Slight snow showers', '🌨️'), 86: ('Heavy snow showers', '🌨️'),
-        95: ('Thunderstorm', '⛈️'),
-        96: ('Thunderstorm, slight hail', '⛈️'), 99: ('Thunderstorm, heavy hail', '⛈️'),
-    }
-    return codes.get(code, ('Unknown', '🌡️'))
-
 def weather(latitude: float, longitude: float) -> dict:
     '''https://open-meteo.com/en/docs#api_documentation'''
     if not latitude or not longitude:
@@ -130,7 +130,8 @@ def weather(latitude: float, longitude: float) -> dict:
     current = data.get('current', {})
     daily = data.get('daily', {})
     round_first_value = lambda var: round(float(daily[var][0])) if (daily.get(var, [None])[0] is not None) else '--'
-    description, emoji = weather_description(code=current.get('weather_code', -1))
+    weather_code=current.get('weather_code', -1)
+    description, emoji = WEATHER_CODES.get(weather_code, ('Unknown', '🌡️'))
     return {
             'temperature': current.get('temperature_2m', '--'),
             'high': round_first_value('temperature_2m_max'),
@@ -139,6 +140,8 @@ def weather(latitude: float, longitude: float) -> dict:
             'description': description,
             'emoji': emoji,
             'location': resolve_location(LATITUDE, LONGITUDE),
+            'current_url': f'https://www.yr.no/en/forecast/hourly-table/{latitude},{longitude}',
+            'forecast_url': f'https://www.yr.no/en/forecast/daily-table/{latitude},{longitude}',
             }
 
 
@@ -148,8 +151,7 @@ class Handler(http.server.BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
-            html_path = pathlib.Path(__file__).parent / 'index.html'
-            self.wfile.write(html_path.read_bytes())
+            self.wfile.write(INDEX_HTML)
         elif self.path == '/api/nowplaying':
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
@@ -172,13 +174,13 @@ class Handler(http.server.BaseHTTPRequestHandler):
 def run():
     server_address = ('', PORT)
     httpd = http.server.ThreadingHTTPServer(server_address, Handler)
-    logging.info(f"Starting NowPlaying on http://localhost:{PORT}")
-    logging.info(f"Tracking Last.fm user: {LASTFM_USER}")
+    logging.info(f'Starting NowPlaying on http://localhost:{PORT}')
+    logging.info(f'Tracking Last.fm user: {LASTFM_USER}')
 
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
-        logging.info("Shutting down server...")
+        logging.info('Shutting down server...')
         httpd.server_close()
 
 
