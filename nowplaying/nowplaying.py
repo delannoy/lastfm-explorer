546bd6d41850756bf39ae58903c8c1d31771c211
Andres G. Delannoy <delannoy@cern.ch>
2026-09-12 16:05:39 +0200

    added lastfm and musicbrainz hyperlinks

--- nowplaying/nowplaying.py
+++ nowplaying/nowplaying.py
@@ -48,10 +48,10 @@ def lastfm_nowplaying(user: str, api_key: str) -> dict[str, int|bool|str]:
     tracks = data.get('recenttracks', {}).get('track', [])
     if not tracks:
         return {'error': 'No tracks found'}
-    total_listens = data.get('recenttracks', {}).get('@attr', {}).get('total', 0)
     track = tracks[0]
     images = track.get('image', [])
     artist_name, album_name, track_name = track.get('artist', {}).get('#text', ''), track.get('album', {}).get('#text', ''), track.get('name', '')
+    artist_mbid, album_mbid, track_mbid = track.get('artist', {}).get('mbid', ''), track.get('album', {}).get('mbid', ''), track.get('mbid', '')
     stats = lastfm_track_stats(user, api_key, track_name, artist_name)
     return {
         'playing': (track.get('@attr', {}).get('nowplaying') == 'true'),
@@ -59,23 +59,35 @@ def lastfm_nowplaying(user: str, api_key: str) -> dict[str, int|bool|str]:
         'album': album_name,
         'track': track_name,
         'image': images[-1].get('#text', '') if images else '',
-        'total_listens': total_listens,
+        'total_listens': data.get('recenttracks', {}).get('@attr', {}).get('total', 0),
         'track_listens': stats['track_listens'],
         'loved': stats['loved'],
+        'user_url': f"https://www.last.fm/user/{urllib.parse.quote_plus(user)}",
+        'artist_url': stats['artist_url'],
+        'album_url': stats['album_url'],
+        'track_url': stats['track_url'],
+        'track_library_url': f"https://www.last.fm/user/{urllib.parse.quote_plus(user)}/library/music/{urllib.parse.quote_plus(artist_name)}/_/{urllib.parse.quote_plus(track_name)}",
+        'artist_mb_url': f'https://musicbrainz.org/artist/{artist_mbid}' if artist_mbid else '',
+        'album_mb_url': f'https://musicbrainz.org/release/{album_mbid}' if album_mbid else '',
+        'track_mb_url': f'https://musicbrainz.org/recording/{track_mbid}' if track_mbid else '',
     }
 
-def lastfm_track_stats(user: str, api_key: str, track_name: str, artist_name: str) -> dict[str, int|bool]:
+def lastfm_track_stats(user: str, api_key: str, track_name: str, artist_name: str) -> dict[str, int|bool|str]:
     '''Fetches stats (playcount, loved) for a specific track from LASTFM.'''
+    default_stats = {'track_listens': 0, 'loved': False, 'track_url': '', 'artist_url': '', 'album_url': ''}
     if not track_name or not artist_name:
-        return {'track_listens': 0, 'loved': False}
+        return default_stats
     url = f'https://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key={api_key}&artist={urllib.parse.quote(artist_name)}&track={urllib.parse.quote(track_name)}&username={user}&format=json'
     data = query(url=url)
     if 'error' in data:
-        return {'track_listens': 0, 'loved': False}
+        return default_stats
     track_info = data.get('track', {})
     return {
         'track_listens': int(track_info.get('userplaycount', 0)),
-        'loved': int(track_info.get('userloved', 0)) == 1
+        'loved': (int(track_info.get('userloved', 0)) == 1),
+        'track_url': track_info.get('url', ''),
+        'artist_url': track_info.get('artist', {}).get('url', ''),
+        'album_url': track_info.get('album', {}).get('url', '')
     }
 
 @functools.lru_cache(maxsize=1)
